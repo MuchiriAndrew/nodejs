9a5987c3b0bdb089cebb29e4c1cc6fd437eff5cd
MuchiriAndrew <kariukia225@gmail.com>
2023-05-11 14:17:09 +0300

    first commit

---
+++ global.js
@@ -0,0 +1,20 @@
+//Global Object
+
+// console.log(global);
+
+// setTimeout(() => {
+//     console.log('in the timeout');
+//     clearInterval(int);
+// }, 3000);
+
+
+// const int = setInterval(() => {
+//     console.log('in the interval');
+// }, 1000);
+//cancel out the process by Ctrl-C
+
+// console.log(__dirname);//gives the absolute directory name
+// console.log(__filename);//gives the absolute file name
+
+
+
