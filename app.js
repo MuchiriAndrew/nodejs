4c3cb4ac0d95db76249afc760c1f9ccc32c8acc5
MuchiriAndrew <kariukia225@gmail.com>
2023-05-19 15:07:34 +0300

    update

--- app.js
+++ app.js
@@ -12,23 +12,24 @@ app.listen(3000);
 
 app.get('/', (req, res) => {
     // res.send('<p> HomePage </p>');
-    res.sendFile('./views/index.html', { root:__dirname });
+    // res.sendFile('./views/index.html', { root:__dirname });
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
     // res.send('<p> About </p>');
-    res.sendFile('./views/about.html', { root:__dirname });
+    // res.sendFile('./views/about.html', { root:__dirname });
+    res.render('about');
 });
 
-//redirects
-
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
-});
+app.get('/blogs/create', (req, res) => {
+    res.render('create');
+})
+   
 
 //404 Page
 
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root:__dirname })
+    res.status(404).render('404');
 });//works like the default case...it's put in last to be the one that is carried out if a match is not found
 
